29de6e86fbd05bb87ad907507ddbdc96d73611c9
tjy-gitnub <2017225905@qq.com>
2022-09-11 17:08:11 +0800

    外观优化，新增任务栏

--- desktop.js
+++ desktop.js
@@ -1,23 +1,29 @@
 function showwin(name) {
     $('.window.' + name).addClass('show-begin');
-    setTimeout(() => {$('.window.' + name).addClass('show');}, 0);
-    setTimeout(() => {$('.window.' + name).addClass('notrans');}, 200);
+    $('#taskbar').attr('count', Number($('#taskbar').attr('count')) + 1)
+    $('#taskbar').html($('#taskbar').html() + '<a class="' + name + '"><img src="icon/' + name + '.png"></a>');
+    if ($('#taskbar').attr('count') == '1') $('#taskbar').show();
+    setTimeout(() => { $('.window.' + name).addClass('show'); }, 0);
+    setTimeout(() => { $('.window.' + name).addClass('notrans'); }, 200);
+}
+function hidewin(name) {
+    $('.window.' + name).removeClass('notrans');
+    $('.window.' + name).removeClass('show');
+    $('#taskbar').attr('count', Number($('#taskbar').attr('count')) - 1)
+    $('#taskbar>.' + name).remove();
+    if ($('#taskbar').attr('count') == '0') $('#taskbar').hide();
+    setTimeout(() => { $('.window.' + name).removeClass('show-begin'); }, 200);
 }
 function maxwin(name) {
     if ($('.window.' + name).hasClass('max')) {
         $('.window.' + name).removeClass('max');
-        setTimeout(() => {$('.window.' + name).addClass('notrans');}, 200);
+        setTimeout(() => { $('.window.' + name).addClass('notrans'); }, 200);
         $('.window.' + name + '>.titbar>div>.wbtg.max').html('<i class="bi bi-app"></i>');
     } else {
         $('.window.' + name).removeClass('notrans');
         $('.window.' + name).addClass('max');
         $('.window.' + name + '>.titbar>div>.wbtg.max').html('<svg version="1.1" width="12" height="12" viewBox="0,0,37.65105,35.84556" style="margin-top:4px;"><g transform="translate(-221.17804,-161.33903)"><g data-paper-data="{&quot;isPaintingLayer&quot;:true}" fill="none" fill-rule="nonzero" stroke="#000000" stroke-width="2" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" style="mix-blend-mode: normal"><path d="M224.68734,195.6846c-2.07955,-2.10903 -2.00902,-6.3576 -2.00902,-6.3576l0,-13.72831c0,0 -0.23986,-1.64534 2.00902,-4.69202c1.97975,-2.68208 4.91067,-2.00902 4.91067,-2.00902h14.06315c0,0 3.77086,-0.23314 5.80411,1.67418c2.03325,1.90732 1.33935,5.02685 1.33935,5.02685v13.39347c0,0 0.74377,4.01543 -1.33935,6.3576c-2.08312,2.34217 -5.80411,1.67418 -5.80411,1.67418h-13.39347c0,0 -3.50079,0.76968 -5.58035,-1.33935z"/><path d="M229.7952,162.85325h16.06111c0,0 5.96092,-0.36854 9.17505,2.64653c3.21412,3.01506 2.11723,7.94638 2.11723,7.94638v18.55642"/></g></g></svg>')
     }
-}
-function hidewin(name) {
-    $('.window.' + name).removeClass('notrans');
-    $('.window.' + name).removeClass('show');
-    setTimeout(() => {$('.window.' + name).removeClass('show-begin');}, 200);
 }
 function showdropdown(dropid, type1, pos1, type2, pos2) {
     var dd = document.getElementById(dropid);
