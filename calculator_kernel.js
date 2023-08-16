9ff417af1ee3d6bf20367308e2b47bd184e1acec
lh11117 <99455215+lh11117@users.noreply.github.com>
2023-08-16 10:15:56 +0800

    Update calculator_kernel.js
    格式化代码

--- calculator_kernel.js
+++ calculator_kernel.js
@@ -1,157 +1,157 @@
 
-var num1 = 0,num2 = 0;
+var num1 = 0, num2 = 0;
 var c = 0;
 
-function get_num(id){
+function get_num(id) {
     return Number(document.getElementById(id).innerHTML);
 }
 
 
-function on_click_number_key(key,id){
+function on_click_number_key(key, id) {
     //key:按下的数字键，int
     //id:显示区的id
-    if (document.getElementById(id).innerHTML == "0"){
+    if (document.getElementById(id).innerHTML == "0") {
         document.getElementById(id).innerHTML = ""
     }
     document.getElementById(id).innerHTML += key;
 }
 
-function on_click_fun_key(key,id){
+function on_click_fun_key(key, id) {
     //key:按下的功能键，int,加1，减2，乘3，除4
     num1 = Number(document.getElementById(id).innerHTML);
     c = key;
     document.getElementById(id).innerHTML = "0"
 }
 
-function on_click_point(id){
-    if(document.getElementById(id).innerHTML == ""){
+function on_click_point(id) {
+    if (document.getElementById(id).innerHTML == "") {
         document.getElementById(id).innerHTML = "0"
     }
-    if (!(document.getElementById(id).innerHTML.includes('.'))){
+    if (!(document.getElementById(id).innerHTML.includes('.'))) {
         document.getElementById(id).innerHTML += "."
     }
 }
 
-function kernel_Square(id){
-    document.getElementById(id).innerHTML = Math.pow(get_num(id),2)
+function kernel_Square(id) {
+    document.getElementById(id).innerHTML = Math.pow(get_num(id), 2)
 }
 
-function kernel_SquareRoot(id){
+function kernel_SquareRoot(id) {
     document.getElementById(id).innerHTML = Math.sqrt(get_num(id))
 }
 
-function backspace(id){
-    if(document.getElementById(id).innerHTML.length>0){
-        document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.substring(0,document.getElementById(id).innerHTML.length-1)
+function backspace(id) {
+    if (document.getElementById(id).innerHTML.length > 0) {
+        document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.substring(0, document.getElementById(id).innerHTML.length - 1)
     }
-    if (document.getElementById(id).innerHTML==""){
+    if (document.getElementById(id).innerHTML == "") {
         document.getElementById(id).innerHTML = "0"
     }
 }
 
 
-function clear_num(id){
+function clear_num(id) {
     document.getElementById(id).innerHTML = "0"
 }
 
-function on_click_eq(id){
+function on_click_eq(id) {
     num2 = Number(document.getElementById(id).innerHTML);
-    document.getElementById(id).innerHTML = _calc(num1,num2,c).toString()
+    document.getElementById(id).innerHTML = _calc(num1, num2, c).toString()
 }
 
 function accAdd(arg1, arg2) {
     var r1, r2, m, c;
     try {
-           r1 = arg1.toString().split(".")[1].length;
+        r1 = arg1.toString().split(".")[1].length;
     }
     catch (e) {
-           r1 = 0;
+        r1 = 0;
     }
     try {
-           r2 = arg2.toString().split(".")[1].length;
+        r2 = arg2.toString().split(".")[1].length;
     }
     catch (e) {
-           r2 = 0;
+        r2 = 0;
     }
-       c = Math.abs(r1 - r2);
-       m = Math.pow(10, Math.max(r1, r2));
+    c = Math.abs(r1 - r2);
+    m = Math.pow(10, Math.max(r1, r2));
     if (c > 0) {
-    var cm = Math.pow(10, c);
-    if (r1 > r2) {
-               arg1 = Number(arg1.toString().replace(".", ""));
-               arg2 = Number(arg2.toString().replace(".", "")) * cm;
+        var cm = Math.pow(10, c);
+        if (r1 > r2) {
+            arg1 = Number(arg1.toString().replace(".", ""));
+            arg2 = Number(arg2.toString().replace(".", "")) * cm;
+        } else {
+            arg1 = Number(arg1.toString().replace(".", "")) * cm;
+            arg2 = Number(arg2.toString().replace(".", ""));
+        }
     } else {
-               arg1 = Number(arg1.toString().replace(".", "")) * cm;
-               arg2 = Number(arg2.toString().replace(".", ""));
-    }
-    } else {
-           arg1 = Number(arg1.toString().replace(".", ""));
-           arg2 = Number(arg2.toString().replace(".", ""));
+        arg1 = Number(arg1.toString().replace(".", ""));
+        arg2 = Number(arg2.toString().replace(".", ""));
     }
     return (arg1 + arg2) / m;
-   }
-   function accSub(arg1, arg2) {
+}
+function accSub(arg1, arg2) {
     var r1, r2, m, n;
     try {
-           r1 = arg1.toString().split(".")[1].length;
+        r1 = arg1.toString().split(".")[1].length;
     }
     catch (e) {
-           r1 = 0;
+        r1 = 0;
     }
     try {
-           r2 = arg2.toString().split(".")[1].length;
+        r2 = arg2.toString().split(".")[1].length;
     }
     catch (e) {
-           r2 = 0;
+        r2 = 0;
     }
-       m = Math.pow(10, Math.max(r1, r2)); //last modify by deeka //动态控制精度长度
-       n = (r1 >= r2) ? r1 : r2;
+    m = Math.pow(10, Math.max(r1, r2)); //last modify by deeka //动态控制精度长度
+    n = (r1 >= r2) ? r1 : r2;
     return ((arg1 * m - arg2 * m) / m).toFixed(n);
-   }
-   function accMul(arg1, arg2) {
+}
+function accMul(arg1, arg2) {
     var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
     try {
-           m += s1.split(".")[1].length;
+        m += s1.split(".")[1].length;
     }
     catch (e) {
     }
     try {
-           m += s2.split(".")[1].length;
+        m += s2.split(".")[1].length;
     }
     catch (e) {
     }
     return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
-   }
+}
 
-   function accDiv(arg1, arg2) {
+function accDiv(arg1, arg2) {
     var t1 = 0, t2 = 0, r1, r2;
     try {
-           t1 = arg1.toString().split(".")[1].length;
+        t1 = arg1.toString().split(".")[1].length;
     }
     catch (e) {
     }
     try {
-           t2 = arg2.toString().split(".")[1].length;
+        t2 = arg2.toString().split(".")[1].length;
     }
     catch (e) {
     }
     with (Math) {
-           r1 = Number(arg1.toString().replace(".", ""));
-           r2 = Number(arg2.toString().replace(".", ""));
-    return (r1 / r2) * pow(10, t2 - t1);
+        r1 = Number(arg1.toString().replace(".", ""));
+        r2 = Number(arg2.toString().replace(".", ""));
+        return (r1 / r2) * pow(10, t2 - t1);
     }
-   }
+}
 
 
-function _calc(n1,n2,c){
-    switch(c){
+function _calc(n1, n2, c) {
+    switch (c) {
         case 1:
-            return accAdd(n1,n2);
+            return accAdd(n1, n2);
         case 2:
-            return accSub(n1,n2);
+            return accSub(n1, n2);
         case 3:
-            return accMul(n1,n2);
+            return accMul(n1, n2);
         case 4:
-            return accDiv(n1,n2);
+            return accDiv(n1, n2);
     }
 }
