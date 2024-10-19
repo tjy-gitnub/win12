c8d9a846920477a4b37835b5c5cf11587c59ba9d
Program <ProgramCX@Outlook.com>
2024-10-20 00:33:01 +0800

    可以通过按下meta键呼出开始菜单
    使用Vue 3监听meta键按下事件，然后启动开始菜单

---
+++ scripts/hotkey.js
@@ -0,0 +1,27 @@
+﻿const desktop={
+    methods: {
+        metaPressed(){
+            console.log("metaPressed");
+            if($('#start-menu').hasClass('show')){
+                hide_startmenu();
+            }
+            else{
+                $('#start-btn').addClass('show');
+                if($('#search-win').hasClass('show')){
+                    $('#search-btn').removeClass('show');
+                    $('#search-win').removeClass('show');
+                    setTimeout(() => {
+                        $('#search-win').removeClass('show-begin');
+                    }, 200);
+                    hide_widgets();
+                }
+                if($('#widgets').hasClass('show'))hide_widgets();
+                $('#start-menu').addClass('show-begin');
+                setTimeout(() => {
+                    $('#start-menu').addClass('show');
+                }, 0);
+            }
+        }
+    },
+};
+Vue.createApp(desktop).mount("#desktop");
