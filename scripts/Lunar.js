c804adad51e7dcb97b48d277919fb8fe1fdf5cf5
lingbopro <ilovemc@zohomail.cn>
2026-02-18 16:49:20 +0800

    更好的农历日期获取

--- scripts/Lunar.js
+++ scripts/Lunar.js
@@ -1,22 +1,34 @@
 //农历获取实现 By @stxttkx
-async function getLunar()
-{
-    var lunarDisplay = document.getElementById('lunar');
-    try{
-        const response = await fetch('https://api.lolimi.cn/API/rl/api');
-        if (!response.ok){
-            throw new Error('网络不给力');
+// 由 lingbopro 改良
+
+let lunarCache = null;
+async function getLunar() {
+    let lunarDisplay = document.getElementById('lunar');
+    if (!lunarCache) {
+        try {
+            const response = await fetch('https://api.lolimi.cn/API/rl/api');
+            if (!response.ok) {
+                // 你是怎么想出来往开发控制台打印这种东西的。。。
+                // throw new Error('网络不给力');
+                throw new Error('HTTP 状态码不符合预期: ' + response.status);
+            }
+            const jsonContent = await response.json();
+            if (jsonContent.code !== 1) {
+                throw new Error(`服务器异常，错误码: ${jsonContent.code}`);
+            }
+            if (typeof jsonContent?.data?.lunar !== 'string') {
+                throw new Error('服务器返回数据格式异常');
+            }
+
+            lunarCache = jsonContent;
+        } catch (error) {
+            lunarDisplay.textContent = '';
+            console.error('获取农历信息失败: ', error);
         }
-        const jsonContent = await response.json();
-        if(jsonContent.code !== 1){
-            throw new Error(`服务器异常，错误码: ${jsonContent.code}`);
-        }
-        const lunarContent = jsonContent?.data?.lunar
+    }
+
+    if (lunarCache?.data?.lunar) {
+        const lunarContent = lunarCache.data.lunar;
         lunarDisplay.textContent = lunarContent;
     }
-    catch(error){
-        lunarDisplay.textContent = error;
-    }
 }
-
-getLunar();//启动模块
